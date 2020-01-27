751c3172e00c1e073151d5a06a94f63b6a5f5d02
new2dmancode <60340189+new2dmancode@users.noreply.github.com>
2020-01-27 12:19:06 +0300

    Update man.py
    add age Vluber

--- HomeWork/man.py
+++ HomeWork/man.py
@@ -1,5 +1,7 @@
 ##print('Hello World')
 print('Hello World ..!')
 name = input("Give me your name: ")
-print("Your name is " + name)
-print('Hello' + name)
+print("Your name is..: " + name)
+print("your age is..: " + age)
+print('Hello ' + name)
+
